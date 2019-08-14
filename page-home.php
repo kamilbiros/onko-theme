bce39a0ec6447f6ed7d4867466b4a41646ec927c
Kamil Biros <kamilbiros@Kamils-MacBook-Pro.local>
2019-08-14 21:43:59 +0200

    you have to install module Contact Form 7 and add your custom contact form in wordpress, than copy shortcode and pase in page-home.php in contact section

--- page-home.php
+++ page-home.php
@@ -129,6 +129,31 @@ $about_button_link = get_field('button_field_link');
   </section>
 
 <!-- EOF ABOUT SECTION -->
+<!-- CONTACT SECTION -->
+<section class="page-section" id="contact">
+    <div class="container">
+
+      <!-- Contact Section Heading -->
+		<h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Contact Me</h2>
+
+		<!-- Icon Divider -->
+		<div class="divider-custom">
+			<div class="divider-custom-line"></div>
+			<div class="divider-custom-icon">
+				<i class="fas fa-star"></i>
+			</div>
+			<div class="divider-custom-line"></div>
+		</div>
+		<div class="row">
+        	<div class="col-lg-8 mx-auto">
+
+				<?php echo do_shortcode( '[contact-form-7 id="99" title="Contact"]') ?>
+
+			</div>
+		</div>
+    </div>
+</section>
+<!-- EOF CONTACT SECTION -->
 
 <?php
 // get_sidebar();
